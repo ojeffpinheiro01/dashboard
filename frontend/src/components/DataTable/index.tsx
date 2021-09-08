bf266512338c4464c4c1627ae9b5cad6091e6a05
ojeffpinheiro <jefersonpinheirodesouza@gmail.com>
2021-09-08 00:06:51 -0300

    add componentes básicos

---
+++ frontend/src/components/DataTable/index.tsx
@@ -0,0 +1,94 @@
+function DataTable() {
+    return (
+        <div className="table-responsive">
+            <table className="table table-striped table-sm">
+                <thead>
+                    <tr>
+                        <th>Data</th>
+                        <th>Client</th>
+                        <th>Visits</th>
+                        <th>Trainings</th>
+                        <th>Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>John Cenna</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Todo Duro</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Mike Tyson</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Monde Tyler</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Nelson Farit</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Zé do bolo</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Muqueca Silva</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Magro de Melo</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Silva Sousa Soares</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Parell Wilame</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+
+
+    );
+}
+
+export default DataTable;
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,25 @@
+function Footer() {
+  return (
+    <footer className="footer mt-auto py-3 bg-dark">
+      <div className="container">
+        <p className="text-light">
+          Developed with Love by{" "}
+          <a href="https://github.com/ojeffpinheiro01" target="_blank" rel="noreferrer" >
+              Jéferson Pinheiro
+            </a>
+        </p>
+        <p className="text-light">
+          <small>
+            <strong>Spring React Week</strong> <br />
+            Evento promovido pela escola DevSuperior:{" "}
+            <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer" >
+                @devsuperior.ig
+            </a>
+          </small>
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
