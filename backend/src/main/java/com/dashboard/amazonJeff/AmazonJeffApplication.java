e2da18f7d6a18c2e5058810c56ced30de1f7991d
ojeffpinheiro <jefersonpinheirodesouza@gmail.com>
2021-09-07 21:47:23 -0300

    estrutura inicial do projeto

---
+++ backend/src/main/java/com/dashboard/amazonJeff/AmazonJeffApplication.java
@@ -0,0 +1,13 @@
+package com.dashboard.amazonJeff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonJeffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazonJeffApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dashboard/amazonJeff/AmazonJeffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dashboard.amazonJeff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonJeffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
