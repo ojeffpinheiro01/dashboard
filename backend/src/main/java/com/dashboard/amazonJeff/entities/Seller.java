34957d305cb5eb619399a27540be8e21903d1fc7
ojeffpinheiro <jefersonpinheirodesouza@gmail.com>
2021-09-11 13:36:37 -0300

    primeira homogolação

--- backend/src/main/java/com/dashboard/amazonJeff/entities/Seller.java
+++ backend/src/main/java/com/dashboard/amazonJeff/entities/Seller.java
@@ -21,9 +21,7 @@ public class Seller {
 	@OneToMany(mappedBy = "seller")
 	private List<Sale> sales = new ArrayList<>();
 	
-	public Seller() {
-		
-	}
+	public Seller() {}
 
 	public Seller(Long id, String name) {
 		this.id = id;
